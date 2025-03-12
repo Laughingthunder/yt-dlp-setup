f1702610feaab9697ba2b2d5d31a797303956ec0
Laughingthunder <laughingthunder26@gmail.com>
2025-03-12 13:50:57 -0500

    Update readme and add main files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# yt-dlp-setup
-Download and update yt-dlp and the build of ffmpeg developed for it.
+# yt-dlp Setup
+A simple, public-domain batch script to automatically download or update yt-dlp and its ffmpeg dependencies for MPC-HC and similar use-cases.
+## Usage
+Just run `yt-dlp_setup.bat`. It'll automatically detect your system (32 or 64-bit) and download or update yt-dlp and its dependencies accordingly.
+## License
+This script is released into the public domain, matching [yt-dlp's license](https://github.com/yt-dlp/yt-dlp/blob/master/LICENSE).
+## Dependencies:
+All dependencies are GPL-licensed, and have been included for convenience.
+- wget.exe (included or easily found online)
+- 7za.exe (7-Zip command-line tool)
---
+++ yt-dlp-setup/yt-dlp_setup.bat
@@ -0,0 +1,69 @@
+@echo off
+REM yt-dlp Setup: Download and update yt-dlp.
+title "yt-dlp Setup"
+setlocal
+if defined PROCESSOR_ARCHITEW6432 (
+    set "ARCH=x64"
+    set "ARCHBITS=64"
+    set "YTDLPURL=https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe"
+    set "YTDLPFILENAME=yt-dlp.exe"
+) else if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    set "ARCH=x64"
+    set "ARCHBITS=64"
+    set "YTDLPURL=https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe"
+    set "YTDLPFILENAME=yt-dlp.exe"
+) else (
+    set "ARCH=x86"
+    set "ARCHBITS=32"
+    set "YTDLPURL=https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_%ARCH%.exe"
+    set "YTDLPFILENAME=yt-dlp_%ARCH%.exe"
+)
+if not exist "%~dp0%YTDLPFILENAME%" (
+    timeout /t 2 /nobreak >nul
+    ECHO.
+    ECHO "Starting yt-dlp Setup..."
+    timeout /t 2 /nobreak >nul
+    ECHO "Done."
+    ECHO.
+    ECHO ""%YTDLPFILENAME%" does not exist. Fetching "%YTDLPFILENAME%"..."
+    "%~dp0%ARCH%\\wget.exe" --no-hsts --no-verbose -P "%~dp0." "%YTDLPURL%" 2>nul
+    if exist "%~dp0%YTDLPFILENAME%" (
+        ECHO "Done."
+        ECHO.
+    ) else (
+        ECHO "ERROR: Failed to download yt-dlp.exe. Press any key to exit."
+        pause >nul
+        exit /b 1
+    )
+) else (
+    if exist "%~dp0%YTDLPFILENAME%" (
+        timeout /t 2 /nobreak >nul
+        ECHO.
+        ECHO "Starting yt-dlp Setup..."
+        timeout /t 2 /nobreak >nul
+        ECHO "Done."
+        ECHO.
+    )
+)
+ECHO "Checking for available yt-dlp updates..."
+ECHO "We'll only update if necessary."
+"%~dp0%YTDLPFILENAME%" -U
+ECHO "Done."
+ECHO.
+ECHO "Checking for yt-dlp ffmpeg..."
+ECHO "We'll only download it if necessary."
+"%~dp0%ARCH%\\wget.exe" -N --no-hsts --no-verbose --content-disposition -P "%~dp0ffmpeg_dl" "https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win%ARCHBITS%-gpl.zip" 2>nul
+ECHO "Done."
+ECHO.
+ECHO "Extracting yt-dlp ffmpeg..."
+"%~dp0%ARCH%\\7za.exe" e "%~dp0ffmpeg_dl\ffmpeg-master-latest-win%ARCHBITS%-gpl.zip" ^
+    "ffmpeg-master-latest-win%ARCHBITS%-gpl\bin\ffmpeg.exe" ^
+    "ffmpeg-master-latest-win%ARCHBITS%-gpl\bin\ffplay.exe" ^
+    "ffmpeg-master-latest-win%ARCHBITS%-gpl\bin\ffprobe.exe" ^
+    -o"%~dp0." -y >nul
+ECHO "Done."
+ECHO.
+endlocal
+ECHO "yt-dlp Setup has completed successfully. Press any key to exit."
+pause >nul
+exit
